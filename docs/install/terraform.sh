0f28b3e69d3faa3b2639232eebcc9a9135ee2863
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2020-05-30 23:34:40 -0500

    agregando kubectl

--- docs/install/terraform.sh
+++ docs/install/terraform.sh
@@ -5,5 +5,5 @@ version="0.12.26"
 cd /opt && wget https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip
 unzip terraform_${version}_linux_amd64.zip
 chmod +x terraform
-sudo mv terraform /usr/sbin/
+mv terraform /usr/sbin/
 rm -rf terraform_${version}_linux_amd64.zip
